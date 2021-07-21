e0c0be30d0efcf702ddb42b771b31c0b3b4a69d9
Vinoth Kumar R <vinxter@gmail.com>
2021-07-20 23:17:48 -0700

    Initial Commit

---
+++ HRWatchAgent WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HRWatchAgent WatchKit Extension
+//
+//  Created by Vinoth Kumar on 20/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HRWatchAgent WatchKit Extension/HRWatchAgentApp.swift
@@ -0,0 +1,19 @@
+//
+//  HRWatchAgentApp.swift
+//  HRWatchAgent WatchKit Extension
+//
+//  Created by Vinoth Kumar on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct HRWatchAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
